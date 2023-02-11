c0afd65a3768e957ebab11600bd2e273961c1c0a
Antonio <antonio.webdesign19@gmail.com>
2023-02-11 07:37:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card";
+import Data from "./dummyData";
+
+function App() {
+  const cards = Data.map((obj) => {
+    return <Card img={obj.img} title={obj.title} subtitle={obj.subtitle} />;
+  });
+  return (
+    <div className="bg-slate-900 px-8 py-32 poppins">
+      <h1 className="text-gray-50 font-semibold text-2xl mb-4">
+        Macbooks Ecommerce
+      </h1>
+      <p className="text-gray-50 text-sm mb-12">
+        Click on any of these cards’ button and you will be redirected to its
+        own route.
+      </p>
+      <div>{cards}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dummyData.js
@@ -0,0 +1,44 @@
+const Data = [
+  {
+    title: "Macbook 1",
+    subtitle: "a simple macbook",
+    description: "this will be generated",
+    features: ["feat1", "feat2", "feat3"],
+    price: 236,
+    img: "https://source.unsplash.com/ztvxSNFJih0",
+  },
+  {
+    title: "Macbook 2",
+    subtitle: "a simple macbook",
+    description: "this will be generated",
+    features: ["feat1", "feat2", "feat3"],
+    price: 236,
+    img: "https://source.unsplash.com/alCEnNmzhPE",
+  },
+  {
+    title: "Macbook 3",
+    subtitle: "a simple macbook",
+    description: "this will be generated",
+    features: ["feat1", "feat2", "feat3"],
+    price: 236,
+    img: "https://source.unsplash.com/q2fY5MGh1CM",
+  },
+  {
+    title: "Macbook 4",
+    subtitle: "a simple macbook",
+    description: "this will be generated",
+    features: ["feat1", "feat2", "feat3"],
+    price: 236,
+    img: "https://source.unsplash.com/Im7lZjxeLhg",
+  },
+  {
+    title: "Macbook 5",
+    subtitle: "a simple macbook",
+    description: "this will be generated",
+    features: ["feat1", "feat2", "feat3"],
+    price: 236,
+    img: "https://source.unsplash.com/y0_vFxOHayg",
+  },
+];
+
+export default Data;
